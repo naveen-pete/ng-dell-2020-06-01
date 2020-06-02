e2c61a5369775413af5dcf84c89f8e8513d5cdd1
naveen-pete <naveen.shenoy.blr@gmail.com>
2020-06-02 15:53:55 +0530

    use material components in login, also add reactive form validation for login

--- apps/day-02/fitness-app/src/app/auth/login/login.component.ts
+++ apps/day-02/fitness-app/src/app/auth/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
@@ -6,10 +7,23 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
+  form: FormGroup;
 
   constructor() { }
 
   ngOnInit(): void {
+    this.form = new FormGroup({
+      email: new FormControl('', { validators: [Validators.required, Validators.email] }),
+      password: new FormControl('', Validators.required)
+    });
+  }
+
+  onSubmit() {
+    if (this.form.invalid) {
+      return;
+    }
+
+    console.log('form value:', this.form.value);
   }
 
 }
