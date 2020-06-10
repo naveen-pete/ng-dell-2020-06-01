8dc0f92731a77e6997407a9b5636399785fa7678
naveen-pete <naveen.shenoy.blr@gmail.com>
2020-06-10 10:49:35 +0530

    add modularity and lazy loading to the app

--- apps/day-08/fitness-app/src/app/app-routing.module.ts
+++ apps/day-08/fitness-app/src/app/app-routing.module.ts
@@ -2,17 +2,15 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 import { HomeComponent } from './home/home.component';
-import { TrainingComponent } from './training/training.component';
-import { LoginComponent } from './auth/login/login.component';
-import { SignupComponent } from './auth/signup/signup.component';
 import { AuthGuard } from './auth/auth.guard';
-import { CheckForDataGuard } from './auth/check-for-data.guard';
 
 const appRoutes: Routes = [
   { path: '', component: HomeComponent },
-  { path: 'training', component: TrainingComponent, canActivate: [AuthGuard] },
-  { path: 'login', component: LoginComponent },
-  { path: 'signup', component: SignupComponent },
+  {
+    path: 'training',
+    loadChildren: () => import('./training/training.module').then(m => m.TrainingModule),
+    canLoad: [AuthGuard]
+  },
   { path: '**', redirectTo: '' }
 ];
 
--- apps/day-08/fitness-app/src/app/auth/auth.guard.ts
+++ apps/day-08/fitness-app/src/app/auth/auth.guard.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, UrlTree, Router } from '@angular/router';
+import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, UrlTree, Router, CanLoad, Route, UrlSegment } from '@angular/router';
 import { Observable } from 'rxjs';
 import { map, take } from 'rxjs/operators';
 
@@ -8,27 +8,35 @@ import { AuthService } from './auth.service';
 @Injectable({
   providedIn: 'root'
 })
-export class AuthGuard implements CanActivate {
+export class AuthGuard implements CanActivate, CanLoad {
 
   constructor(
     private authService: AuthService,
     private router: Router
   ) { }
 
+  canLoad(route: Route, segments: UrlSegment[]): boolean | Observable<boolean> | Promise<boolean> {
+    return this.checkIfAuthenticated();
+  }
+
   canActivate(
     next: ActivatedRouteSnapshot,
     state: RouterStateSnapshot
   ): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
+    return this.checkIfAuthenticated();
+  }
 
+  private checkIfAuthenticated() {
     return this.authService.user.pipe(
       take(1),
       map(user => {
         if (user) {
           return true;
         }
-        return this.router.createUrlTree(['/login']);
+        this.router.navigate(['/login']);
+        return false;
       })
     );
-
   }
+
 }
