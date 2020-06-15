24885a7774efd1ae9d8860328d42438a0c8e0c74
naveen-pete <naveen.shenoy.blr@gmail.com>
2020-06-15 21:34:37 +0530

    add initial app for app shell demo

---
+++ apps/helper-apps/fitness-app-shell/src/app/navigation/sidenav-list/sidenav-list.component.ts
@@ -0,0 +1,42 @@
+import { Component, OnInit, Output, EventEmitter, OnDestroy } from '@angular/core';
+import { Subscription } from 'rxjs';
+
+import { AuthService } from '../../auth/auth.service';
+
+@Component({
+  selector: 'app-sidenav-list',
+  templateUrl: './sidenav-list.component.html',
+  styleUrls: ['./sidenav-list.component.css']
+})
+export class SidenavListComponent implements OnInit, OnDestroy {
+  @Output() closeSidenav = new EventEmitter<void>();
+
+  isAuth = false;
+  private authSub: Subscription;
+
+  constructor(private authService: AuthService) { }
+
+  ngOnInit() {
+    this.authSub = this.authService.user.subscribe(
+      user => {
+        this.isAuth = !!user;
+      }
+    );
+  }
+
+  onClose() {
+    this.closeSidenav.emit();
+  }
+
+  onLogout() {
+    this.onClose();
+    this.authService.logout();
+  }
+
+  ngOnDestroy() {
+    if (this.authSub) {
+      this.authSub.unsubscribe();
+    }
+  }
+
+}
---
+++ apps/helper-apps/fitness-app-shell/src/app/shared/shared.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+
+import { CommonModule } from '@angular/common';
+import { FlexLayoutModule } from '@angular/flex-layout';
+import { MaterialModule } from '../material.module';
+
+@NgModule({
+  exports: [
+    CommonModule,
+    FlexLayoutModule,
+    MaterialModule
+  ]
+})
+export class SharedModule { }
