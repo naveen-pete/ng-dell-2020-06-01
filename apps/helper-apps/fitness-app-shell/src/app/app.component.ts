6b8a9b5ca7f0c686efd959fea50818520bc44fa1
naveen-pete <naveen.shenoy.blr@gmail.com>
2020-06-15 22:19:08 +0530

    add app shell to the app

--- apps/helper-apps/fitness-app-shell/src/app/app.component.ts
+++ apps/helper-apps/fitness-app-shell/src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Inject, PLATFORM_ID } from '@angular/core';
+import { isPlatformBrowser } from "@angular/common";
 
 import { AuthService } from './auth/auth.service';
 
@@ -10,9 +11,16 @@ import { AuthService } from './auth/auth.service';
 export class AppComponent implements OnInit {
   title = 'fitness-app';
 
-  constructor(private authService: AuthService) { }
+  constructor(
+    private authService: AuthService,
+    @Inject(PLATFORM_ID) private platformId: any
+  ) { }
 
   ngOnInit() {
-    this.authService.autoLogin();
+    if (isPlatformBrowser(this.platformId)) {
+      this.authService.autoLogin();
+    }
+
+    console.log('AppComponent.ngOnInit() invoked.');
   }
 }
