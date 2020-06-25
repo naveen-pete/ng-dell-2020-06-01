ba14d3666c119b4cdd5dc348fbbf116b2f55d965
naveen-pete <naveen.shenoy.blr@gmail.com>
2020-06-25 07:24:22 +0530

    commit demo app for micro frontends

---
+++ apps/helper-apps/mf-demo/container/src/dell-root-config.js
@@ -0,0 +1,34 @@
+import { registerApplication, start } from "single-spa";
+
+registerApplication({
+  name: "@dell/navbar",
+  app: () => System.import("@dell/navbar"),
+  activeWhen: () => {
+    return true;
+  },
+  customProps: {
+    domElement: document.getElementById("mf-header"),
+  }
+});
+
+registerApplication({
+  name: "@dell/home",
+  app: () => System.import("@dell/home"),
+  activeWhen: (location) => location.pathname === "/",
+  customProps: {
+    domElement: document.getElementById("mf-content"),
+  },
+});
+
+registerApplication({
+  name: "@dell/users",
+  app: () => System.import("@dell/users"),
+  activeWhen: (location) => location.pathname.startsWith("/users"),
+  customProps: {
+    domElement: document.getElementById("mf-content"),
+  },
+});
+
+start({
+  urlRerouteOnly: true,
+});
