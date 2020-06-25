b9d2d27ccf9333ca969756bc315cca83d8024c96
naveen-pete <naveen.shenoy.blr@gmail.com>
2020-06-25 07:34:19 +0530

    remove mf-app from the repo

--- apps/helper-apps/mf-app/users/src/app/empty-route/empty-route.component.ts
+++
@@ -1,7 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'users-empty-route',
-  template: '',
-})
-export class EmptyRouteComponent {}
--- apps/helper-apps/mf-app/users/src/app/services/user.model.ts
+++
@@ -1,7 +0,0 @@
-export interface UserModel {
-  id: number;
-  email: string;
-  firstName: string;
-  lastName: string;
-  avatar: string;
-}
